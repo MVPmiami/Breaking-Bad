9fbd188f7efdf8557b4660c3d9dca39a665d99c5
Aliaksei Siankevich <andfly@tut.by>
2021-10-22 15:21:25 +0300

    P-5: fixed store

--- src/components/organisms/CardList/container.js
+++ src/components/organisms/CardList/container.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getPersonsList } from "./../../../store/actions/persons";
+import React from "react";
+import { useSelector } from "react-redux";
 import CardList from "./component";
 
 export const PersonsListContainer = () => {
-  const dispatch = useDispatch();
   const persons = useSelector((state) => state.cardListReducer.cardList);
-
-  useEffect(() => {
-    dispatch(getPersonsList());
-  }, [dispatch]);
-
   return <CardList persons={persons} />;
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore } from "redux";
 import reducers from "./reducers";
- 
 const store = createStore(reducers);
 export default store;
